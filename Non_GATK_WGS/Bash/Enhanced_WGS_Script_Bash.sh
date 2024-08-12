e976fd92eda957ff72a50c34c71b48a0073fe620
Sombuddha-Roy-Bhowmick <119097830+Sombuddha-Roy-Bhowmick@users.noreply.github.com>
2024-08-12 22:12:03 +0530

    Update Enhanced_WGS_Script_Bash.sh

--- Non_GATK_WGS/Bash/Enhanced_WGS_Script_Bash.sh
+++ Non_GATK_WGS/Bash/Enhanced_WGS_Script_Bash.sh
@@ -69,13 +69,13 @@ parallel -j 2 < $out/parallel_fastp # Running fastp  in parallel
 
 #Alignment & Markduplication of Normal Sample
 
-/data/007_Softwares/Isaac4/Isaac-build/bin/isaac-align -r $Isaac_hg38  -b $out/$input1/fastp_output -f fastq --use-bases-mask y150,y150 -m200 -j $threads --keep-duplicates 1 --mark-duplicates 1  -o $out/$input1/Alignment -t $out/$input1/Alignment/Temp
+/data/softwares/Isaac4/Isaac-build/bin/isaac-align -r $Isaac_hg38  -b $out/$input1/fastp_output -f fastq --use-bases-mask y150,y150 -m200 -j $threads --keep-duplicates 1 --mark-duplicates 1  -o $out/$input1/Alignment -t $out/$input1/Alignment/Temp
 
 rm -rf $out/$input1/Alignment/Temp
 
 #Alignment & Markduplication of Tumor Sample
 
-/data/007_Softwares/Isaac4/Isaac-build/bin/isaac-align -r $Isaac_hg38 -b $out/$input2/fastp_output -f fastq --use-bases-mask y150,y150 -m200 -j $threads  --keep-duplicates 1 --mark-duplicates 1  -o $out/$input2/Alignment -t $out/$input2/Alignment/Temp
+/data/softwares/Isaac4/Isaac-build/bin/isaac-align -r $Isaac_hg38 -b $out/$input2/fastp_output -f fastq --use-bases-mask y150,y150 -m200 -j $threads  --keep-duplicates 1 --mark-duplicates 1  -o $out/$input2/Alignment -t $out/$input2/Alignment/Temp
 
 rm -rf $out/$input2/Alignment/Temp
 
@@ -99,23 +99,23 @@ eval "$(conda shell.bash hook)"         # Setting bash for conda environment
 conda deactivate             # Deactivating conda environment
 
 eval "$(conda shell.bash hook)"		# Setting bash for conda environment
-conda activate py27		# Activating conda environment
+conda activate py2		# Activating conda environment
 
 #SV Caller
 
-python /data/007_Softwares/manta-1.6.0.centos6_x86_64/bin/configManta.py --normalBam $out/$input1/BQSR/${input1}.bqsr.bam --tumourBam $out/$input2/BQSR/${input2}.bqsr.bam --referenceFasta $Ref_hg38  --runDir $out/variantCaller/Manta
+python /data/softwares/manta-1.6.0.centos6_x86_64/bin/configManta.py --normalBam $out/$input1/BQSR/${input1}.bqsr.bam --tumourBam $out/$input2/BQSR/${input2}.bqsr.bam --referenceFasta $Ref_hg38  --runDir $out/variantCaller/Manta
 
 python $out/variantCaller/Manta/runWorkflow.py  -j $threads
 
 #Somatic Variant Caller
 
-python /data/007_Softwares/strelka-2.9.10.centos6_x86_64/bin/configureStrelkaSomaticWorkflow.py --normalBam $out/$input1/BQSR/${input1}.bqsr.bam --tumourBam $out/$input2/BQSR/${input2}.bqsr.bam --referenceFasta $Ref_hg38 --indelCandidates $out/variantCaller/Manta/results/variants/candidateSmallIndels.vcf.gz  --runDir $out/variantCaller/Strelka2_Somatic 
+python /data/softwares/strelka-2.9.10.centos6_x86_64/bin/configureStrelkaSomaticWorkflow.py --normalBam $out/$input1/BQSR/${input1}.bqsr.bam --tumourBam $out/$input2/BQSR/${input2}.bqsr.bam --referenceFasta $Ref_hg38 --indelCandidates $out/variantCaller/Manta/results/variants/candidateSmallIndels.vcf.gz  --runDir $out/variantCaller/Strelka2_Somatic 
 
 python $out/variantCaller/Strelka2_Somatic/runWorkflow.py -m local -j $threads
 
 #Germline Variant Caller
 
-python /data/007_Softwares/strelka-2.9.10.centos6_x86_64/bin/configureStrelkaGermlineWorkflow.py --bam $out/$input2/BQSR/${input1}.bqsr.bam --referenceFasta $Ref_hg38  --runDir $out/variantCaller/Strelka2_Germline
+python /data/softwares/strelka-2.9.10.centos6_x86_64/bin/configureStrelkaGermlineWorkflow.py --bam $out/$input2/BQSR/${input1}.bqsr.bam --referenceFasta $Ref_hg38  --runDir $out/variantCaller/Strelka2_Germline
 
 python $out/variantCaller/Strelka2_Germline/runWorkflow.py -m local -j $threads
 
